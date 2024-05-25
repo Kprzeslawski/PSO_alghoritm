cd5ef80c2f75463df40d1cba5f214d58acb2340c
Krzysztof <kprzeslawski01@gmail.com>
2024-05-25 13:46:06 +0200

    Initial commit

---
+++ testFunctions.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Krzysztof on 25.05.2024.
+//
+
+#include "testFunctions.h"
+
+#include <stdexcept>
+
+double testFunctions::fun1(double *x, int count) {
+    if(count < 2)throw std::runtime_error("Invalid args count");
+    return ((x[0] - 5) * (x[0] - 1) + 12) * ((x[1] - 2) * (x[1] - 3) + 4);
+}
---
+++ testFunctions.h
@@ -0,0 +1,21 @@
+//
+// Created by Krzysztof on 25.05.2024.
+//
+
+#ifndef TESTFUNCTIONS_H
+#define TESTFUNCTIONS_H
+
+#ifndef D double
+#define D double
+#endif
+
+class testFunctions {
+
+public:
+    static D fun1(D* x, int count);
+
+};
+
+
+
+#endif //TESTFUNCTIONS_H
