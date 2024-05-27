0ff29835eef2812160616db1f1595caef56e57df
Krzysztof <kprzeslawski01@gmail.com>
2024-05-27 15:36:43 +0200

    working code

--- PSO.cpp
+++ PSO.cpp
@@ -22,7 +22,7 @@ D * PSO::optimize(
     for (int i = 0; i < count; ++i)
         best_solution[i] = (constraints[i][0] + constraints[i][1]) * .5;
 
-    auto solution_value = fun(best_solution, count);
+    auto best_solution_value = fun(best_solution, count);
 
     //generate particle
     for(int i = 0; i < particle_number; i++) {
@@ -32,47 +32,58 @@ D * PSO::optimize(
         auto * particle_velocity = new D[count];
 
         for(int i2 = 0; i2 < count; i2++) {
-            particle_position[i2] = RN(constraints[i]);
-            particle_velocity[i2] = RN(constraints[i]) * rsv;
+            particle_position[i2] = RN(constraints[i2]);
+            particle_velocity[i2] = RN(constraints[i2]) * rsv;
             best_particle_position[i2] = particle_position[i2];
         }
-        auto best_value = fun(best_particle_position, count);
+        auto best_particle_value = fun(best_particle_position, count);
 
-        while (true) {
+        for(int moves = 0; moves < 1000; moves++) {
             for(int i3 = 0; i3 < count; i3++) {
                 particle_velocity[i3] =
                     inertia * particle_velocity[i3] +
-                    RN() * cop * (best_particle_position[i] - particle_position[i]) +
-                    RN() * sios * (best_particle_position[i] - particle_position[i]);
+                    RN() * cop * (best_particle_position[i3] - particle_position[i3]) +
+                    RN() * sios * (best_particle_position[i3] - particle_position[i3]);
                 particle_position[i3] += particle_velocity[i3];
 
                 if(particle_position[i3] < constraints[i3][0])particle_position[i3] = constraints[i3][0];
                 else if(particle_position[i3] > constraints[i3][1])particle_position[i3] = constraints[i3][1];
             }
 
+            if(fun(particle_position,count) < best_particle_value) {
+                best_particle_value = fun(particle_position,count);
+                for(int i3 = 0; i3 < count; i3++)
+                    best_particle_position[i3] = particle_position[i3];
+            }
+            if(fun(particle_position,count) < best_solution_value) {
+                best_solution_value = fun(particle_position,count);
+                for(int i3 = 0; i3 < count; i3++)
+                    best_solution[i3] = particle_position[i3];
+            }
 
         }
     }
+
     return best_solution;
 }
 
 
 D PSO::RN() {
-    std::uniform_real_distribution<auto> unif(0,1);
+    std::uniform_real_distribution<D> unif(0.,1.);
     std::random_device r;
     std::default_random_engine re(r());
     return unif(re);
 }
 
 D PSO::RN(D * c) {
-    std::uniform_real_distribution<auto> unif(c[0],c[1]);
+    std::uniform_real_distribution<D> unif(c[0],c[1]);
     std::random_device r;
     std::default_random_engine re(r());
     return unif(re);
 }
 
 D PSO::RN(D a, D b) {
-    std::uniform_real_distribution<auto> unif(a,b);
+    std::uniform_real_distribution<D> unif(a,b);
     std::random_device r;
     std::default_random_engine re(r());
     return unif(re);
--- main.cpp
+++ main.cpp
@@ -13,10 +13,11 @@ int main() {
     std::cout << "Random value from constraints: "<< PSO::RN(f_con[0]) << std::endl;
 
     auto alg = PSO();
+    auto sol_vect = alg.optimize(testFunctions::fun1,f_con,2,1000,0.8,0.1,0.1,0.1);
 
     std::cout << "F(0,0): "<< testFunctions::fun1(arr,2) << std::endl;
     std::cout << "F - optimal: "
-        << testFunctions::fun1(alg.optimize(testFunctions::fun1,f_con,2,1000,0.8,0.1,0.1),2)
-        << std::endl;
+        << testFunctions::fun1(sol_vect,2)
+        << " At: " << sol_vect[0] << ", " << sol_vect[1] << std::endl;
     return 0;
 }
