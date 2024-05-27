24e4567be2528ef6abcc70d1a5ff528db3ead8b1
Krzysztof <kprzeslawski01@gmail.com>
2024-05-27 17:07:48 +0200

    main cleanup

--- main.cpp
+++ main.cpp
@@ -3,24 +3,31 @@
 #include "PSO.h"
 #include "testFunctions.h"
 
-int main() {
-    double arr[] = {3.,2.};
+void test_1();
+void solve();
 
+int main() {
+    //test_1();
+    solve();
+}
+void solve() {
+
+}
+
+
+void test_1() {
     auto **f_con = new double*[] {
         new double[]{0., 10.},
         new double[]{0., 10.}
     };
-    std::cout << "Random value from constraints: "<< PSO::RN(f_con[0]) << std::endl;
 
     auto alg = PSO();
+
     auto t_0 = clock();
     auto sol_vect = alg.optimize(testFunctions::fun1,f_con,2,10000,0.8,0.1,0.1,0.1);
     std::cout << "T: " << clock() - t_0 << std::endl;
 
-
-    std::cout << "F(0,0): "<< testFunctions::fun1(arr,2) << std::endl;
     std::cout << "F - optimal: "
         << testFunctions::fun1(sol_vect,2)
         << " At: " << sol_vect[0] << ", " << sol_vect[1] << std::endl;
-    return 0;
 }
